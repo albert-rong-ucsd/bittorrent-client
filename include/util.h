bcd64b1e416e2209e9b66c3d0fc0e9ecfb882891
Albert R <arong@ucsd.edu>
2023-03-29 14:01:22 -0700

    Updated util's metainfo structs

--- include/util.h
+++ include/util.h
@@ -3,18 +3,24 @@
 
 #include <time.h>
 
-// currently only supports single file
 struct bt_fileinfo {
 	char *name;                // name of file
 	unsigned long long length; // length of file in bytes
-	int piece_length;          // number of bytes in each piece
-	int num_pieces;            // number of pieces in file (from above 2)
-	char **piece_hashes;       // points to concatenation of 20-byte SHA1 hashes of each piece
+	char *path;                // path to file
+};
+
+struct bt_info {
+	struct bt_fileinfo **f_info; // array of ptrs to info on each file in torrent
+	int num_files;               // number of files in torrent
+	int piece_length;            // number of bytes in each piece
+	int num_pieces;              // number of pieces in file (derived)
+	char **piece_hashes;         // points to concatenation of 20-byte SHA1 hashes of each piece
 };
 
 struct bt_metainfo {
-	struct bt_fileinfo *f_info; // info for torrent file
+	struct bt_info *info;       // info for torrent file
 	char *announce;             // url of tracker
+								// XXX: announce list not included at the moment
 	time_t *created;            // creation time of torrent
 	char *comment;              // comments by author
 	char *creator;              // name + version of program that created .torrent
